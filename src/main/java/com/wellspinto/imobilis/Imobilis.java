ec18e3ba2df63ac5d0cadec8babde1555bbb28bd
YOGA 510 <YOGA 510@DESKTOP-1QNNNVD>
2022-06-07 14:45:43 -0300

    Implementação dos recursos de inicialização --PARAM --LIST ALL | SERACH

--- src/main/java/com/wellspinto/imobilis/Imobilis.java
+++ src/main/java/com/wellspinto/imobilis/Imobilis.java
@@ -1,11 +1,14 @@
 package com.wellspinto.imobilis;
 
 import com.formdev.flatlaf.FlatDarkLaf;
+import com.google.protobuf.ByteString;
 import com.wellspinto.funcoes.DataBasePassWordCrypto;
 import com.wellspinto.funcoes.Globais;
 import java.awt.Color;
 import java.awt.Insets;
 import java.text.DecimalFormat;
+import java.util.prefs.BackingStoreException;
+import javax.swing.SwingConstants;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 
@@ -45,7 +48,7 @@ public class Imobilis {
                         int _port = Integer.parseInt(args[6].toString());
                         String _database = args[7].toString();
                         
-                        System.out.println(args[0].toString() + " " + args[1].toString() + " " + args[2].toString() + " " + _nn +
+                        System.out.println(args[0].toString() + " " + args[1].toString() + " " + args[2].toString() + " " + _nn + " " +
                                 _alias + "," + _host + ":" + _port + "," + _database);
                         Globais.prefs.put("Remote" + _nn, _alias + "," + _host + ":" + _port + "," + _database);
                     } else if (args[2].equalsIgnoreCase("DEL")) {
@@ -66,11 +69,33 @@ public class Imobilis {
                     }
                 } else if (args[1].equalsIgnoreCase("--PARAM")) {
                     if (args[2].equalsIgnoreCase("ADD")) {
-                        String _type = args[3].toString();
+                        String _type = args[3].toString().trim().toUpperCase();
                         String _key = args[4].toString();
                         String _value = args[5].toString();
+                        switch (_type) {
+                            case "STRING":
+                                Globais.prefs.put(_key, _value);
+                                System.out.println("--MAKE --PARAM ADD " + _type + " " + _key + " " + _value + " as sucessfull aditionated.");
+                                break;
+                            case "INTEGER":
+                                Globais.prefs.putInt(_key, Integer.parseInt(_value));
+                                System.out.println("--MAKE --PARAM ADD " + _type + " " + _key + " " + _value + " as sucessfull aditionated.");
+                                break;
+                            case "BOOLEAN":
+                                Globais.prefs.putBoolean(_key, Boolean.valueOf(_value));
+                                System.out.println("--MAKE --PARAM ADD " + _type + " " + _key + " " + _value + " as sucessfull aditionated.");
+                                break;
+                            case "DATE":
+                                Globais.prefs.put(_key, _value);
+                                System.out.println("--MAKE --PARAM ADD " + _type + " " + _key + " " + _value + " as sucessfull aditionated.");
+                                break;
+                            default:
+                                System.out.println("Tipo desconhecido...");
+                        }
                     } else if(args[2].equalsIgnoreCase("DEL")) {
-                    
+                        String _key = args[3].toString();
+                        Globais.prefs.remove(_key);
+                        System.out.println("--MAKE --PARAM DEL " + _key + " as sucessifull removed.");
                     } else {
                         System.out.println("--MAKE --PARAM commands:");
                         System.out.println("====================================");
@@ -80,11 +105,53 @@ public class Imobilis {
                         System.out.println("");
                         System.out.println("Command Sintaxe:");
                         System.out.println("");
-                        System.out.println("-- MAKE --PARAM [ADD|DEL] <<TYPE>> KEY VALUE");
+                        System.out.println("--MAKE --PARAM [ADD|DEL] [<<TYPE>>] KEY VALUE");
                         System.out.println("");
                         System.out.println("<<TYPE>> - STRING; INTEGER; BOOLEAN; DATE");
                         System.out.println("");
                     }
+                }
+            } else if (args[0].equalsIgnoreCase("--PARAM")) {
+                if (args[1].equalsIgnoreCase("--LIST")) {
+                    if (args[2].equalsIgnoreCase("ALL")) {
+                        System.out.println("--PARAM --LIST ALL");
+                        System.out.println("==============================================");
+                        try {
+                            String[] keys = Globais.prefs.keys();
+                            for (String key : keys) {
+                                System.out.println("Key: " + key + " = " + Globais.prefs.get(key, ""));
+                            }
+                        } catch (BackingStoreException e) {}              
+                        System.out.println("==============================================");
+                    } else if (args[2].equalsIgnoreCase("SEARCH")) {
+                        String _search = args[3].toString();
+                        System.out.println("--PARAM --LIST SEARCH " + _search);
+                        System.out.println("==============================================");
+                        try {
+                            String[] keys = Globais.prefs.keys();
+                            boolean _found = false;
+                            for (String key : keys) {
+                                if (key.trim().equalsIgnoreCase(_search)) {
+                                    System.out.println("Key: " + key + " = " + Globais.prefs.get(key, ""));
+                                    _found = true;
+                                }
+                            }
+                            if (!_found) System.out.println("Key: " + _search + " (Not found.");
+                        } catch (BackingStoreException e) {}              
+                        System.out.println("==============================================");
+                    } else {
+                        System.out.println("--PARAM --LIST commands:");
+                        System.out.println("=========================================");
+                        System.out.println("ALL - List all variables of imobilis.");
+                        System.out.println("SEARCH - Search the variable in imobilis.");
+                        System.out.println("-----------------------------------------");
+                        System.out.println("");
+                        System.out.println("Command sintaxe:");
+                        System.out.println("");
+                        System.out.println("--PARAM --LIST ALL|SEARCH [value]");
+                        System.out.println("");
+                    }
+                    System.exit(0);
                 }
             } 
         }
